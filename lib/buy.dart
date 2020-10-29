c619994c7da0a52dbe0842a43e8ab7dd3227ca74
manasvipatidar <mpatidar@cs.iitr.ac.in>
2020-10-29 21:50:46 +0530

    Addes basic login and register

---
+++ lib/buy.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class BuyView extends StatefulWidget {
+  @override
+  _BuyViewState createState() => _BuyViewState();
+}
+
+class _BuyViewState extends State<BuyView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/sell.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SellView extends StatefulWidget {
+  @override
+  _SellViewState createState() => _SellViewState();
+}
+
+class _SellViewState extends State<SellView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
